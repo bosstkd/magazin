ed5eabf46306fef656b668aaec37c4a96b5fb2ff
administrateur <administrateur@PC49.ccckoudiet.local>
2019-06-09 16:16:53 +0100

    whether-map

---
+++ magazin/src/main/java/com/entities/prodSuiv1PK.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+/**
+ *
+ * @author administrateur
+ */
+ @Embeddable
+public class prodSuiv1PK {
+    
+
+    @Column(name = "comment")
+    private String comment;
+
+    @ManyToOne
+    @JoinColumn(name = "idpub")
+    private Publication idpub;
+
+    @ManyToOne
+    @JoinColumn(name = "idu")
+    private Users idu;
+
+}
